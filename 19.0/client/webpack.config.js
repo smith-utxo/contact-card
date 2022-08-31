ad3c1d31d592bd76c96fe692faac4f20182f3372
Adam Smith <smith.adam937@gmail.com>
2022-08-31 11:52:16 -0500

    webpack, loaders, and plugins added

--- 19.0/client/webpack.config.js
+++ 19.0/client/webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ".src/js/index.js", 
+  mode: 'development',
+  entry: '../src/js/index.js',
   output: {
-    filename: "main.js", 
-    path: path.resolve(_dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    title: 'Webpack Plugin',
+  })
+],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
   }
 }
+
+
+
